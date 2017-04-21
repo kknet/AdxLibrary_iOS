1e44ab10cffd55cc344f24467d4c332203b42a74
Chiung Choi <god@malangstudio.com>
2017-04-21 17:51:20 +0900

    [Modify] update Mobvista SDK 1.6.6

--- ADXLibrary/Dependencies/MVSDK.framework/Versions/A/Headers/MVSDK.h
+++ ADXLibrary/Dependencies/MVSDK.framework/Versions/A/Headers/MVSDK.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99e9a17bb3e61ff59266c2d442a1ccbf024256fb01e84e5ec521b34f12670dd6
-size 4274
+oid sha256:2589ccd09c2f1ffe54fac3ec19b01c73da564cc50f7bfd4a91f874659e1192d7
+size 4319
